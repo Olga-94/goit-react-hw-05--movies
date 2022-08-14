250924806e7d0e3690feb584941b2ac4857f579a
Olga-94 <olgabezdetko1994@gmail.com>
2022-08-14 16:18:07 +0300

    PAGINATION

--- src/Views/HomePage.jsx
+++ src/Views/HomePage.jsx
@@ -1,44 +1,51 @@
 
 import { useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
-// import toast from 'react-hot-toast';
+import { useNavigate, useRouteMatch } from 'react-router-dom';
+import toast from 'react-hot-toast';
 import { fetchTrandingMovies } from '../services/apiService';
 import MovieCardList from '../components/MovieCardList/MovieCardList';
 import styled from 'styled-components';
-// import Pagination from '../components/Pagination/Pagination';
+import Pagination from '../components/Pagination/Pagination';
 
  const HomePage = () => {
-  // const history = useHistory();
+  const { isExact } = useRouteMatch();
+  const navigate = useNavigate();
   const [movies, setMovies] = useState([]);
-  // const [totalPages, setTotalPages] = useState(null);
-  // const currentPage =
-  //   Number(new URLSearchParams(window.location.search).get('page')) || 1;
+  const [totalPages, setTotalPages] = useState(null);
+  const currentPage = Number(new URLSearchParams(window.location.search).get('page')) || 1;
   
     useEffect(() => {
-      // if (!isExact) {
-      //   history.push('/');
-      //   toast.error('Page not found', { duration: 3000 });
-      // }
+      if (!isExact) {
+        navigate.push('/');
+        toast.error('Page not found', { duration: 3000 });
+      }
     async function getFetchMovies() {
       try {
-        const data = await fetchTrandingMovies();
-        const { results } = data;
+        const data = await fetchTrandingMovies(currentPage);
+        const { results, total_pages } = data;
 
-        // setTotalPages(total_pages);
+        setTotalPages(total_pages);
         setMovies(results);
       } catch (error) {
         console.log(error);
       }
     }
     getFetchMovies();
-  }, []);
+  }, [currentPage, navigate, isExact]);
+  
+  const handlePageClick = ({ selected }) => {
+    navigate.push({
+      ...window.location,
+      search: selected === 0 ? '' : `page=${selected + 1}`,
+    });
+  };
 
   return (
     <>
      <Title>Trending Today</Title>
       {movies && <MovieCardList movies={movies} />}
       {/* <MovieCardList movies={movies} /> */}
-      {/* <Pagination totalPages={totalPages} onClick={handlePageClick} /> */}
+      <Pagination totalPages={totalPages} onClick={handlePageClick} />
     </>
   );
 }
